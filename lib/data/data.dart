ff004d07aa92266aa9e03f22577dc034371e9691
RedBaron1918 <asatianikotiko1@gmail.com>
2023-03-02 03:38:59 -0800

    added module and data

---
+++ lib/data/data.dart
@@ -0,0 +1,52 @@
+import 'package:clothe_store/models/products.dart';
+
+final List<Product> products = [
+  Product(
+    imageUrl:
+        "https://images.unsplash.com/photo-1611312449412-6cefac5dc3e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=692&q=80",
+    price: 100,
+    productName: "jacketN1",
+    reviews: 20,
+    sold: 50,
+  ),
+  Product(
+    imageUrl:
+        "https://images.unsplash.com/photo-1611312449408-fcece27cdbb7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=669&q=80",
+    price: 100,
+    productName: "jacketN1",
+    reviews: 20,
+    sold: 50,
+  ),
+  Product(
+    imageUrl:
+        "https://images.unsplash.com/photo-1611312449297-a69dc9c3987b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=698&q=80",
+    price: 100,
+    productName: "jacketN1",
+    reviews: 20,
+    sold: 50,
+  ),
+  Product(
+    imageUrl:
+        "https://images.unsplash.com/photo-1434389677669-e08b4cac3105?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=705&q=80",
+    price: 100,
+    productName: "jacketN1",
+    reviews: 20,
+    sold: 50,
+  ),
+  Product(
+    imageUrl:
+        "https://images.unsplash.com/photo-1591047139829-d91aecb6caea?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80",
+    price: 100,
+    productName: "jacketN1",
+    reviews: 20,
+    sold: 50,
+  ),
+  Product(
+    imageUrl:
+        "https://images.unsplash.com/photo-1542272605-15bd6a2bd4f4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    price: 100,
+    productName: "jacketN1",
+    reviews: 20,
+    sold: 50,
+  ),
+];
---
+++ lib/models/products.dart
@@ -0,0 +1,10 @@
+class Product {
+  final String? imageUrl;
+  final int? reviews;
+  final int? sold;
+  final String? productName;
+  final int? price;
+
+  Product(
+      {this.imageUrl, this.price, this.productName, this.reviews, this.sold});
+}
